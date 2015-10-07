df0775c2cc5a26219e639dd58252fe2f38ff8f09
bnhogan01 <Bruce.Hogan001@acad.tri-c.edu>
2015-10-06 20:53:29 -0400

    Blah.

---
+++ Project 3/Project 3/Instructor.cs
@@ -0,0 +1,25 @@
+﻿
+
+{
+    class Instructor
+    {
+               //variable declaration
+            
+            private string Name;
+            private string c
+                CourseName;
+
+
+            // method
+
+            //set student grade
+
+            
+
+
+
+
+
+
+    }
+}
